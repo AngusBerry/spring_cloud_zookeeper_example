95956bb4bbd997987f40358dd1bfaac7bcc682df
AngusBarry <angus.berry@gmail.com>
2018-02-19 10:12:34 -0500

    Code to show equivalent YAML property file usage.

--- src/main/java/com/mycompany/springcloudzkexample/spring_cloud_zookeeper_example/ControllerSpringCloudZK.java
+++ src/main/java/com/mycompany/springcloudzkexample/spring_cloud_zookeeper_example/ControllerSpringCloudZK.java
@@ -14,11 +14,6 @@ import java.net.UnknownHostException;
 @RequestMapping("/api")
 public class ControllerSpringCloudZK {
 
-    //local values directly from application.yml
-    @Value("${spring.cloud.zookeeper.connect-string}")
-    private String springCloudZookeeperConnectString;
-
-
     //Zookeeper config values defined in application.yml
     @Value("${dbURL}")
     private String dbURL;
@@ -30,7 +25,7 @@ public class ControllerSpringCloudZK {
     private String dbPassword;
 
     @Value("${other.nestedproperty}")
-    private String otherNestedProperty;
+    private String otherNestedvalue;
 
 
     @RequestMapping(method = RequestMethod.GET, value = "/showpropertyvalues", produces = "text/plain")
@@ -45,11 +40,11 @@ public class ControllerSpringCloudZK {
 
 
         return "This app is running on " + hostname  + ".\r\n" +
-                "The configuration values are applied from Zookeeper running on " + springCloudZookeeperConnectString + "... \r\n\r\n" +
+                "The configuration values are applied from local YAML property files... \r\n\r\n" +
                 "dbURLproperty = " + dbURL + "\r\n" +
                 "dbUser = " + dbUser + "\r\n" +
                 "dbPassword = " + dbPassword + "\r\n" +
-                "other.nestedproperty = " + otherNestedProperty;
+                "other.nestedvalue = " + otherNestedvalue;
     }
 
 }
--- src/main/java/com/mycompany/springcloudzkexample/spring_cloud_zookeeper_example/SpringCloudZookeeperExampleApplication.java
+++ src/main/java/com/mycompany/springcloudzkexample/spring_cloud_zookeeper_example/SpringCloudZookeeperExampleApplication.java
@@ -1,20 +1,11 @@
 package com.mycompany.springcloudzkexample.spring_cloud_zookeeper_example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+
 @SpringBootApplication
 public class SpringCloudZookeeperExampleApplication {
-
-//    @Autowired
-//    private ControllerSpringCloudZK controllerSpringCloudZK;
 
 	public static void main(String[] args) {
 
